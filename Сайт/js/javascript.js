6ec8772db5e9c556f86be1781aed812d6178f4d1
ChristmasOW <69352148+ChristmasOW@users.noreply.github.com>
2020-08-07 16:15:35 +0300

    Add files via upload

---
+++ Сайт/js/javascript.js
@@ -0,0 +1,29 @@
+﻿$(function(){
+
+	$('body').append('<button class="btn_up" />');
+
+	$('.btn_up').click(function(){
+		$('body').animate({'scrollTop': 0}, 1000);
+		$('html').animate({'scrollTop': 0}, 1000);
+	});
+
+	$(window).scroll(function(){
+    if ($(window).scrollTop() > 300) {
+        $('.btn_up').addClass('active');
+    }
+    else {
+        $('.btn_up').removeClass('active');
+    }
+});
+});	
+$(function(){
+
+	$(window).scroll(function(){
+    if ($(window).scrollTop() > 50) {
+        $('.anim1').addClass('active');
+    }
+    else {
+        $('.anim1').removeClass('active');
+    }
+});
+});	
